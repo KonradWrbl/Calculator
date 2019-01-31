8bbf944d56aa8dfe0740c980d094292e884db767
KonradWrbl <160815@stud.prz.edu.pl>
2019-01-31 20:48:27 +0100

    fexed background

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -4,18 +4,18 @@ $('.button:not(.special)').on('click', function(e) {
     document.form.textview.value += e.target.value;
 });
 
-$('#equal').on('click', function(e) {
+$('#equal').on('click', function() {
     let num = document.form.textview.value;
     if(num) {
         document.form.textview.value = Math.round(eval(num)*1000000000)/1000000000;
     }
 });
 
-$('#C').on('click', function(e) {
+$('#C').on('click', function() {
     document.form.textview.value = '';
 });
 
-$('#back').on('click', function(e) {
+$('#back').on('click', function() {
     let el = document.form.textview.value;
     document.form.textview.value = el.substring(0,el.length-1);
 });
