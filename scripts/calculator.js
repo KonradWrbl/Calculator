322dd3236d51e9d3c15baf0e44ae4b2200b7a8c0
KonradWrbl <160815@stud.prz.edu.pl>
2019-02-15 10:12:52 +0100

    pretty code

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,7 +1,7 @@
 const $ = window.jQuery;
 
 
-$('.button:not(.special)').on('click', function(e) {
+$('.button:not(.special)').on('click', (e) => {
     let val = e.target.value,
         txt = document.form.textview.value;
     if (!(val === txt[txt.length - 1] && (val === '+' || val === '-' || val === '*' || val === '/' || val === '.'))) {
@@ -14,18 +14,18 @@ $('.button:not(.special)').on('click', function(e) {
     }
 });
 
-$('#equal').on('click', function() {
+$('#equal').on('click', () => {
     const num = document.form.textview.value;
     if(num) {
         document.form.textview.value = Math.round(eval(num)*1000000000)/1000000000;
     }
 });
 
-$('#C').on('click', function() {
+$('#C').on('click', () => {
     document.form.textview.value = '';
 });
 
-$('#back').on('click', function() {
+$('#back').on('click', () => {
     const el = document.form.textview.value;
     document.form.textview.value = el.substring(0,el.length-1);
 });
